baed8c1cfdc6f836321eac175728e537161c27b3
droumanet <david.roumanet+tlp@ac-grenoble.fr>
2020-05-18 23:58:18 +0200

    mise à jour

--- javascript/Surprise.js
+++ javascript/Surprise.js
@@ -3,6 +3,9 @@ class Surprise {
         this.description = desc
         this.x = x
         this.y = y
+        // convention : c'est mauvais si Good = 0, c'est bon si Good = 1
+        //              ça continue si End = 0, ça arrête le jeu si End = 1
+        // pour le fun, on colle Good et End dans une seule variable pour utiliser les masques
         this.fin = Good + End<<1
         this.nbreHeure = bonus
     }
--- javascript/game.js
+++ javascript/game.js
@@ -1,19 +1,57 @@
-/* Jeu de chasse au Trésor, version Espace
+/* Ce fichier contient la partie jouable du jeu : 
+   - Initialisation du tableau en mémoire
+   - Calcul des items (bonus ou malus)
+   - traçage du tableau sur la page
+   - récupération des coordonnées et décision
 */
+
+// initialisation des variables
 let nbCaseX = 10
 let nbCaseY = 10
 let nbCase = nbCaseX*nbCaseY
 
-let tableEvent = []
+let tableEvent = [] // tableau des items
 
-// boucle de remplissage du tableau avec des événements de classe Surprise
-
-for (let i=0; i<nbCase*0.1; i++) {
-    let temp = new Surprise("test n°"+i, 5, 5, 0, 0, 0)
-    tableEvent.push(temp)
+// ---- fonctions -----
+function Hasard(max) {
+    // cette fonction calcule un nombre entier aléatoire dans l'intervalle 0 à max
+    return Math.floor(Math.random() * max)
 }
-tableEvent[3].x = 7
-tableEvent.push(4)
+
+function remplissageTable() {
+    // cette fonction créée une liste des items en commençant par le trésor (les autres items doivent éviter ces coordonnées)
+    let tresor = new Surprise("vous avez enfin trouvé la protomolécule", Hasard(nbCaseX), Hasard(nbCaseY), 1, 1, 0 )
+    tableEvent[0] = tresor
+    let nbEvent = nbCase*10/100     // on autorise un max de 10% d'items
+
+    let Good, End, Bonus,cx, cy, temp, phrase
+    let phraseGood=["Vous avez de la chance, vous trouvez un indice qui vous fait gagner un peu de temps", "C'est l'indice qui vous manquait pour vous rapprocher du but", "Hey ! il ne faut jamais lâcher une intuition"]
+    let phraseBad =[]
+
+    // remplissage
+    for (let i=1; i<nbEvent; i++) {
+        Good = Hasard(1)
+        End  = Hasard(1)
+        cx   = Hasard(nbCaseX)
+        cy   = Hasard(nbCaseY)
+        Bonus= Hasard(3)
+
+        // si Good, on pioche une phrase cool et on enlève du temps (calcul se fera pendant le jeu)
+        if (Good) {
+            phrase = phraseGood[Hasard(phraseGood.length)]
+            temp = new Surprise(phrase, cx, cy, Good, End, -bonus)
+        } else {
+            phrase = phraseGood[Hasard(phraseBad.length)]
+            temp = new Surprise(phrase, cx, cy, Good, End, bonus)
+        }
+        tableEvent.push(temp)
+    }
+    tableEvent[3].x = 7
+    tableEvent.push(4)
+}
+
+// TODO: faire un switch/case sur le bonus... 0 indice ne vaut rien, 1 favorise un écart calculé, 2 et 3 pour un indice X ou Y
+// TODO: si Good n'est pas bon, 1 est un contrôle ceinturien, 2 est un abordable pirate, 3 est un assaut martiengit push 
 
 monNombre = "7"
 // affichage du contenu de la collection (liste)
